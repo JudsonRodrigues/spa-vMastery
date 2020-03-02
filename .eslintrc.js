a9963511b6947b0d65f54a0e57ffec572e034670
Judson de Almeida Rodrigues <judson.rodrigues@geap.com.br>
2020-03-02 17:25:39 -0300

    single file component. card de evento, lista de eventos e nav bar

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,14 +3,16 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ["plugin:vue/essential",
+  extends: [
+    "plugin:vue/essential",
     "plugin:prettier/recommended",
-    "@vue/prettier"],
-  parserOptions: {
-    parser: "babel-eslint"
-  },
+    "@vue/prettier"
+  ],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+  },
+  parserOptions: {
+    parser: "babel-eslint"
   }
 };
--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,3 +1,21 @@
 <template>
-  <h1>Events listing</h1>
+  <div>
+    <h1>Events listing</h1>
+
+    <EventCard></EventCard>
+  </div>
 </template>
+
+
+<script>
+	import EventCard from '@/components/EventCard'
+
+	export default {
+		components: {
+			EventCard
+		}
+	}
+</script>
+
+<style scoped>
+</style>
