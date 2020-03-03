d147eadf7b28204ea2503e78b2304554a1cb62f6
Judson de Almeida Rodrigues <judson.rodrigues@geap.com.br>
2020-03-03 11:37:41 -0300

    uso do axios para chamada de servicos. uso do  json-server para mock do server.

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -2,7 +2,7 @@
   <div>
     <h1>Events listing</h1>
 
-    <EventCard></EventCard>
+    <EventCard v-for="event in events" :key="event.id" :event="event"></EventCard>
   </div>
 </template>
 
@@ -10,9 +10,24 @@
 <script>
 	import EventCard from '@/components/EventCard'
 
+	import EventService from '@/services/EventServices'
+
 	export default {
 		components: {
 			EventCard
+		},
+
+		data() {
+			return {
+				events: []
+			}
+		},
+		created() {
+			EventService.getEvents()
+				.then(response => {
+					this.events = response.data
+				})
+				.catch(error => console.log('Houve um erro:' + error.response))
 		}
 	}
 </script>
