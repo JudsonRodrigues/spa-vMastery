be7671bcc00b6d795b30f82fe02a262db2935079
Judson de Almeida Rodrigues <judson.rodrigues@geap.com.br>
2020-03-03 10:12:12 -0300

    Uso básico de slots

--- src/components/BaseIcon.vue
+++ src/components/BaseIcon.vue
@@ -3,6 +3,7 @@
     <svg class="icon" :width="width" :height="heigth">
       <use v-bind="{'xlink:href':'/feather-sprite.svg#'+ name}" />// notice +name here
     </svg>
+    <slot></slot>
   </div>
 </template>
 
